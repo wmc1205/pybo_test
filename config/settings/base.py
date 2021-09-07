a9e427aa7ea7b0c67a8c9e1fdf39f8b6fe2ab340
우민철 <wmc1205@naver.com>
2021-09-07 13:38:37 +0900

    logging 수정

--- config/settings/base.py
+++ config/settings/base.py
@@ -148,7 +148,7 @@ LOGGING = {
             'style': '{',
         },
         'standard': {
-            'format': '%(asctime)s [%(levelname)s] % (name)s : %(message)s'
+            'format': '%(asctime)s [%(levelname)s] %(name)s : %(message)s'
         },
     },
     'handlers': {
